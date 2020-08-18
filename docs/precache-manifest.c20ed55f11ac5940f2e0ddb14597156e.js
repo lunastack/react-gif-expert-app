a4484e4c7d6366c2600f671b1bf104261aa2072d
Nicolas <equipo01@MacBook-Pro-de-Nicolas.local>
2020-08-17 23:13:33 -0400

    docs agregado

---
+++ docs/precache-manifest.c20ed55f11ac5940f2e0ddb14597156e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32994ed83a4607246fe6d8e5d7275728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77bf0d9f22afb01727db",
+    "url": "/static/css/main.52479cf7.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77bf0d9f22afb01727db",
+    "url": "/static/js/main.47723f8f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
